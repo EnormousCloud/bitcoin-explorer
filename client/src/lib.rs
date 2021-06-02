e9544d9eade6a97fc1f661c1c5d59922ff9993d2
EnormousCloud <enormous@webcerebrium.com>
2021-06-03 00:11:15 +0300

    split into workspaces

---
+++ client/src/lib.rs
@@ -0,0 +1,17 @@
+use sauron::prelude::*;
+use sauron::js_sys::TypeError;
+use sauron::web_sys::Response;
+use serde::{
+    Deserialize,
+    Serialize,
+};
+
+#[macro_use]
+extern crate log;
+
+#[wasm_bindgen]
+pub fn main(serialized_state: String) {
+    console_log::init_with_level(log::Level::Trace).unwrap();
+    console_error_panic_hook::set_once();
+    
+}
